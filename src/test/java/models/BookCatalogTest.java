915e2ed765b2bf138f29b184d1063a3d945229eb
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2024-11-25 10:34:36 +0100

    add test files

---
+++ src/test/java/models/BookCatalogTest.java
@@ -0,0 +1,88 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookCatalogTest {
+
+	private BookCatalog bc;
+	private Book book1;
+
+	public BookCatalogTest() {
+		bc = new BookCatalog();
+		Book book1 = new Book(1,"Learning Java","","","",0);
+		bc.addBook(book1);
+	}
+
+	//G
+	@Test
+	public void testAddABook() {
+		Book book = new Book(123,
+				"Book Title",
+				"Book Author",
+				"978-0-306-40615-7",
+				"Book Branch",
+				333);
+		bc.addBook(book);
+		assertEquals(2, bc.getNumberOfBooks(), "The number of books should be 2");
+	}
+
+	//G
+	@Test
+	public void testFindBook()  {
+		Book book = new Book(123,
+				"Book Title",
+				"Book Author",
+				"978-0-306-40615-7",
+				"Book Branch",
+				333);
+		bc.addBook(book);
+        try {
+            assertEquals(book, bc.findBook("Book Title"));
+        } catch (BookNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	//G
+	@Test
+	public void testFindBookIgnoringCase() {
+		Book book = new Book(123,
+				"Book Title",
+				"Book Author",
+				"978-0-306-40615-7",
+				"Book Branch",
+				333);
+		bc.addBook(book);
+        try {
+            assertEquals(book, bc.findBook("book title"));
+        } catch (BookNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	//G
+	@Test
+	public void testFindBookWithExtraSpaces() {
+		Book book = new Book(123,
+				"Book Title",
+				"Book Author",
+				"978-0-306-40615-7",
+				"Book Branch",
+				333);
+		bc.addBook(book);
+		try {
+			assertEquals(book, bc.findBook("  book title  "));
+		} catch (BookNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	//VG
+	// This test should throw BookNotFoundException in order to pass.
+	@Test
+	public void testFindBookThatDoesntExist() throws BookNotFoundException {
+		assertThrows(BookNotFoundException.class, () ->  bc.findBook("No Title"));
+	}
+
+}
